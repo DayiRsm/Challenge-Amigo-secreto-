0db20f47d98092398acfb78fd6a6c028ffdf2f6b
Dayira Rodriguez San Miguel <dayirarsm95@gmail.com>
2025-09-16 13:52:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const imputamigos = document.getElementById("amigo");
-const listaAmigos = [];
+const inputamigos = document.getElementById("amigo"); //variable inputamigos sera el contenedor del elemento obtenido con la fincion getelementbyId
+const listaAmigos = [];// variable constante lista de amigos
 const ullistaAmigos = document.getElementById ("listaAmigos");
 const ulResultado = document.getElementById ("resultado");
-function agregarAmigo(){
-    if (!imputamigos.value){
+function agregarAmigo(){ //funcion que evalua si la variable inputamigos esta: vacia, solo contiene letras, si existe en el arreglo listaAmigos
+    if (!inputamigos.value){
         alert ("Por favor inserte un nombre")
     }
-    listaAmigos.push(imputamigos.value);
+    listaAmigos.push(inputamigos.value);
     console.log(listaAmigos)
-    ullistaAmigos.innerHTML += `<li>${imputamigos.value}</li>`;
+    ullistaAmigos.innerHTML += `<li>${inputamigos.value}</li>`;
 
 }; 
-function sortearAmigo() {
+function sortearAmigo() {//recibe la variable inputamigos y verifica su existencia en la lista de Amigos, retornara verdadero si existe y falso si no se encuentra en la lista
     const random = Math.floor((Math.random() * listaAmigos.length));
     const amigosecreto = listaAmigos[random]
     console.log (random)
     ulResultado.innerHTML = `<li> El amigo secreto es: ${amigosecreto}</li>`;
+
 }
